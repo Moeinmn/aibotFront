401233f6ffd58b812bae33e03b70ff8652e96d86
mahdiyar <mahdiyar@pro.ai>
2023-12-29 05:06:22 -0800

    add navbar

---
+++ src/shared/components/navigations/index.tsx
@@ -0,0 +1,73 @@
+"use client"
+import { useState } from "react";
+import Logo from "./logo";
+import styles from "./styles.module.css"
+
+
+
+
+
+  const Navigation = () => {
+    // State to manage whether the menu is active or not
+    const [isActive, setIsActive] = useState(false);
+
+    // Toggle the active state of the menu
+    const toggleMenu = () => {
+        setIsActive(!isActive);
+    };
+
+    return (
+        <>
+            <div className={styles.header}>
+                <div className={styles.container}>
+                    <nav className={styles.navbar}>
+                        <Logo />
+                        <div 
+                            className={styles.burger} 
+                            id="burger" 
+                            onClick={toggleMenu} // Attach click event to burger
+                        >
+                            <span className={styles.burgerLine}></span>
+                            <span className={styles.burgerLine}></span>
+                            <span className={styles.burgerLine}></span>
+                        </div>
+                        <span 
+                            className={`${styles.overlay} ${isActive ? styles.overlayisactive : ''}`}
+                            onClick={toggleMenu} // Attach click event to overlay
+                        ></span>
+                        <div 
+                            className={`${styles.menu} ${isActive ? styles.menuisactive : ''}`} 
+                            id="menu"
+                        >
+                            <ul className={styles.menuInner}>
+                                {/* Each link closes the menu on click */}
+                                <li className="menuItem">
+                                    <a className={styles.menuLink} href="#" onClick={toggleMenu}>امکانات</a>
+                                </li>
+                                <li className="menuItem">
+                                    <a className={styles.menuLink} href="#" onClick={toggleMenu}>قیمت</a>
+                                </li>
+                                <li className="menuItem">
+                                    <a className={styles.menuLink} href="#" onClick={toggleMenu}>مستندات</a>
+                                </li>
+                                <li className="menuItem">
+                                    <a className={styles.menuLink} href="#" onClick={toggleMenu}>درباره ی ما</a>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div>
+                        <a href="./login" >
+                          ورود</a>
+                        <a href="./login" className={styles.menuBlock}>ثبت نام</a>
+                        </div>
+
+                    </nav>
+                </div>
+            </div>
+        </>
+    );
+};
+
+
+export default Navigation;
---
+++ src/shared/components/navigations/logo.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+const Logo = () => {
+  const router = useRouter();
+
+  return ( 
+    <Image
+      onClick={() => router.push('/')}
+      className="hidden md:block cursor-pointer" 
+      src="/logo.svg" 
+      height="120" 
+      width="120" 
+      alt="Logo" 
+    />
+   );
+}
+ 
+export default Logo;
+
+
