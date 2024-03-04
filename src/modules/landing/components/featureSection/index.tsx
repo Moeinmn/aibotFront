cd1501ae8b2d3022782ed8e8117a4613b373c861
moein moeinnia <moein@pro.ai>
2024-03-04 16:31:15 +0330

    grid added

--- src/modules/landing/components/featureSection/index.tsx
+++ src/modules/landing/components/featureSection/index.tsx
@@ -1,19 +1,16 @@
-"use client"
+"use client";
 import useSelectModal from "@/src/hooks/useSelectModal";
-import styles from './styles.module.css'
-import { Grid, Typography } from "@material-ui/core";
+import styles from "./styles.module.css";
 import Image from "next/image";
-import { Box } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
+import Link from "next/link";
+import ItemsGrid from "../ItemsGrid";
 
-
-
-
-
-const FeatureSection = ()=>{
-
-    return (
-        <div style={{marginTop : "200px"}}>
-    {/* <div className={styles.root}>
+const FeatureSection = () => {
+  return (
+    <Container maxWidth="xl">
+    <div style={{ marginTop: "200px" }}>
+      {/* <div className={styles.root}>
     <div className={styles.continer}>
      <h5>
         <p className={styles.introtext}>
@@ -27,49 +24,102 @@ const FeatureSection = ()=>{
      </h5>
     </div>
           </div> */}
-          <Grid container spacing={2}>
-        
-        <Grid item xs={12} md={6} style={{display: "flex" , flexDirection:"row"}}>
-        <div className={styles.linePath}>
-  <div className={styles.boxRotate}>
-    {/* <Image 
+      <Grid container spacing={2}>
+        <Grid
+          item
+          xs={12}
+          md={6}
+          style={{ display: "flex", flexDirection: "row" }}
+        >
+          <div className={styles.linePath}>
+            <div className={styles.boxRotate}>
+              <Image 
+              width={200}
+              height={200}
     src="https://assets-global.website-files.com/635c4eeb78332f7971255095/63d7184b737d36c33ca81413_LayoutIcon.svg"
      loading="lazy" alt="window icon" className="icon-path" 
-     /> */}
-  </div>
-  <div className={styles.connectorPath}></div>
-</div>
-          <Box>
-          <Typography variant="h2" className="big">
-            Notre monde tourne autour de la rotation
-          </Typography>
-          <Typography
-            variant="body1"
-            className="content text-darker-green-grey"
-          >
-            <div>
-              <div>
-                Les roulements ont contribué à la croissance des industries, ont
-                révolutionné les transports et ont amélioré la qualité de vie
-                globale des humains. Et SKF a participé à une grande partie du
-                processus.
-              </div>
-              <div>&nbsp;</div>
-              <div>
-                Au fil du temps, nous avons repoussé les limites du possible. Et
-                aujourd’hui, nous continuons à développer des technologies de
-                roulement qui soutiennent le progrès et contribuent à améliorer
-                la vie quotidienne, aujourd’hui et demain.
-              </div>
+     />
             </div>
-          </Typography>
+            <div className={styles.connectorPath}></div>
+          </div>
+          <Box>
+            <Typography variant="h2" className="big">
+              لورم ایپسوم متن ساختگی با تولید
+            </Typography>
+            <Typography
+              variant="body1"
+              className="content text-darker-green-grey"
+            >
+              <div>
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+                استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
+                در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد
+                نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
+                کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان
+                جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را
+                برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در
+                زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و
+                دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و
+                زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات
+                پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+              </div>
+            </Typography>
           </Box>
         </Grid>
         <Grid item xs={12} md={6}>
+        <ItemsGrid/>
         </Grid>
       </Grid>
-        </div>
-    )
-}
+      {/* <Grid container spacing={2}>
+        <Grid
+          item
+          xs={12}
+          md={6}
+          style={{ display: "flex", flexDirection: "row" }}
+        >
+          <div className={styles.linePath}>
+            <div className={styles.boxRotate}>
+              <Image 
+              width={200}
+              height={200}
+    src="https://assets-global.website-files.com/635c4eeb78332f7971255095/63d71b1e1938080cef242fea_flowIcon.svg"
+     loading="lazy" alt="window icon" className="icon-path" 
+     />
+            </div>
+            <div className={styles.connectorPath}></div>
+          </div>
+          <Box>
+            <Typography variant="h2" className="big">
+              لورم ایپسوم متن ساختگی با تولید
+            </Typography>
+            <Typography
+              variant="body1"
+              className="content text-darker-green-grey"
+            >
+              <div>
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+                استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
+                در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد
+                نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
+                کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان
+                جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را
+                برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در
+                زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و
+                دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و
+                زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات
+                پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+              </div>
+            </Typography>
+            <Link href={"#"}>
+            سلام ->
+            </Link>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6}></Grid>
+      </Grid> */}
+    </div>
+    </Container>
+  );
+};
 
 export default FeatureSection;
