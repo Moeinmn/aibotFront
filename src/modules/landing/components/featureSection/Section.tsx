2fcfb2e4d9a1e248665261e2539b741d7c592d9c
Moein Moeinnia <Moeinnia@protonmail.com>
2024-04-27 00:43:49 +0330

    chore: features segregated

---
+++ src/modules/landing/components/featureSection/Section.tsx
@@ -0,0 +1,102 @@
+"use client"
+import styles from "./styles.module.css";
+import Image from "next/image";
+import { Box, Container, Grid, Typography } from "@mui/material";
+import { motion, useAnimation, useInView } from 'framer-motion'
+import { useEffect, useRef } from "react";
+
+const Section = ({children , sectionName , title , desc , imgURL , mask}:{
+    children : React.ReactNode , sectionName:string,title:string,desc:string,imgURL:string,mask?:boolean
+}) => {
+    const mainRef = useRef(null);
+    const isInView = useInView(mainRef, { once: true });
+  
+    const animController = useAnimation();
+
+    useEffect(()=>{
+        if(isInView){
+          animController.start("visible")
+        }
+      },[isInView])
+  
+    return(
+        <Grid container spacing={2}>
+        <Grid
+          ref={mainRef}
+          item
+          xs={12}
+          md={6}
+          style={{ display: "flex", flexDirection: "row" }}
+        >
+          <div className={styles.linePath}>
+            <div className={styles.boxRotate}>
+              <Image
+                width={200}
+                height={200}
+                src={imgURL}
+                loading="lazy"
+                alt="window icon"
+                className="icon-path"
+              />
+            </div>
+            <motion.div 
+
+            variants={{
+                hidden: { height:0 },
+                visible: { height:"100%" }
+              }}
+              initial="hidden"
+              animate={animController}
+              transition={{duration: 2}}
+
+            className={styles.connectorPath}>
+
+            </motion.div>
+          </div>
+          <Box>
+          <div className="head-section ">
+            <motion.p 
+                                      variants={{
+                                        hidden: {opacity:0 , x:"-20%"},
+                                        visible: {opacity:1 , x:0}
+                                      }}
+                                      initial="hidden"
+                                      animate={animController}
+                                      transition={{duration: 1 , delay: 0}}
+            style={{color: "#5470ff"}} className="leading-[150%]">{sectionName}</motion.p>
+          </div>
+            <motion.h2 
+                                      variants={{
+                                        hidden: {opacity:0 , x:"-20%"},
+                                        visible: {opacity:1 , x:0}
+                                      }}
+                                      initial="hidden"
+                                      animate={animController}
+                                      transition={{duration: 1 , delay: .6}}
+            className="text-2xl leading-[150%] font-bold big ">
+            {title}
+            </motion.h2>
+
+            <motion.p 
+                                      variants={{
+                                        hidden: {opacity:0 , x:"-20%"},
+                                        visible: {opacity:1 , x:0}
+                                      }}
+                                      initial="hidden"
+                                      animate={animController}
+                                      transition={{duration: 1 , delay: 1.2}}
+            style={{color: "gray"}} className="mt-3 leading-[150%] text-base text-darker-green-grey content">
+             {desc}
+             </motion.p>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6} className={
+            mask ? styles.itemsContainer : ""
+        }>
+          {children}
+        </Grid>
+      </Grid>
+    )
+}
+
+export default Section;
--- src/modules/landing/components/featureSection/index.tsx
+++ src/modules/landing/components/featureSection/index.tsx
@@ -8,6 +8,7 @@ import ItemsGrid from "../ItemsGrid";
 import IntegrationGrid from "../IntegrationGrid";
 import { motion } from 'framer-motion'
 import KnowledgeBaseGrid from "../knowledgeBaseGrid";
+import Section from "./Section";
 
 const FeatureSection = () => {
   return (
@@ -20,207 +21,46 @@ const FeatureSection = () => {
             دارید.
           </h2>
         </div>
-        <Grid container spacing={2} mt={4}>
-          <Grid
-            item
-            xs={12}
-            md={6}
-            style={{ display: "flex", flexDirection: "row" }}
-          >
-            <div className={styles.linePath}>
-              <div className={styles.boxRotate}>
-                <Image
-                  width={200}
-                  height={200}
-                  src="https://assets-global.website-files.com/635c4eeb78332f7971255095/657c74b7a8381409c81fcd56_zapIcon.svg"
-                  loading="lazy"
-                  alt="window icon"
-                  className="icon-path"
-                />
-              </div>
-              <motion.div 
-              initial={{ height:0 }}
-              whileInView={{
-                height:"100%"
-              }}
-              transition={{ duration: 3 , delay: 0 }}
-              className={styles.connectorPath}>
+        <Section sectionName="دانش" title="
+             یک وبسایت یا سند را بارگذاری کنید تا ربات شما آموزش داده شود.
+              "
+        desc="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+        استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
+        در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد
+        نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
 
-              </motion.div>
-            </div>
-            
-            <Box>
-            <div className="head-section ">
-              <p style={{color: "#5470ff"}} className="leading-[150%]">دانش</p>
-            </div>
-              <h2 className="text-2xl leading-[150%] font-bold big ">
-              یک وبسایت یا سند را بارگذاری کنید تا ربات شما آموزش داده شود.
-              </h2>
+      "
+      imgURL="https://assets-global.website-files.com/635c4eeb78332f7971255095/657c74b7a8381409c81fcd56_zapIcon.svg"
+      >
+        <KnowledgeBaseGrid/>
+        </Section>
+        <Section sectionName="دانش" title="
+              لورم ایپسوم متن ساختگی با تولید.
+              "
+        desc="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+        استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
+        در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد
+        نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
 
-              <p style={{color: "gray"}} className="mt-3 leading-[150%] text-base text-darker-green-grey content">
-                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
-                استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
-                در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد
-                نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
-
-              </p>
-            </Box>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            {/* <Image
-              layout="fill"
-              src="https://assets-global.website-files.com/635c4eeb78332f7971255095/65eeb1836d1cabac275fe48d_Import%20Knowledge-p-800.png"
-              loading="lazy"
-              alt="window icon"
-              className="icon-path"
-            /> */}
-            <KnowledgeBaseGrid/>
-          </Grid>
-        </Grid>
-        <Grid container spacing={2} mt={4}>
-          <Grid
-            item
-            xs={12}
-            md={6}
-            style={{ display: "flex", flexDirection: "row" }}
-          >
-            <div className={styles.linePath}>
-              <div className={styles.boxRotate}>
-                <Image
-                  width={200}
-                  height={200}
-                  src="https://assets-global.website-files.com/635c4eeb78332f7971255095/63d7184b737d36c33ca81413_LayoutIcon.svg"
-                  loading="lazy"
-                  alt="window icon"
-                  className="icon-path"
-                />
-              </div>
-              <motion.div 
-              initial={{ height:0 }}
-              whileInView={{
-                height:"100%"
-              }}
-              transition={{ duration: 3 , delay: 0 }}
-              className={styles.connectorPath}>
-
-              </motion.div>
-            </div>
-            <Box>
-            <div className="head-section ">
-              <p style={{color: "#5470ff"}} className="leading-[150%]">دانش</p>
-            </div>
-              <h2 className="text-2xl leading-[150%] font-bold big ">
-                لورم ایپسوم متن ساختگی با تولید
-              </h2>
-
-              <p style={{color: "gray"}} className="mt-3 leading-[150%] text-base text-darker-green-grey content">
-                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
-                استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
-                در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد
-                نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
-
-              </p>
-            </Box>
-          </Grid>
-          <Grid item xs={12} md={6} className={styles.itemsContainer}>
-            <ItemsGrid />
-          </Grid>
-        </Grid>
-        <Grid container spacing={2}>
-          <Grid
-            item
-            xs={12}
-            md={6}
-            style={{ display: "flex", flexDirection: "row" }}
-          >
-            <div className={styles.linePath}>
-              <div className={styles.boxRotate}>
-                <Image
-                  width={200}
-                  height={200}
-                  src="https://assets-global.website-files.com/635c4eeb78332f7971255095/63d71b1e1938080cef242fea_flowIcon.svg"
-                  loading="lazy"
-                  alt="window icon"
-                  className="icon-path"
-                />
-              </div>
-              <motion.div 
-              initial={{ height:0 }}
-              whileInView={{
-                height:"100%"
-              }}
-              transition={{ duration: 3 , delay: 0 }}
-              className={styles.connectorPath}>
-
-              </motion.div>
-            </div>
-            <Box>
-            <div className="head-section ">
-              <p style={{color: "#5470ff"}} className="leading-[150%]">اتصال</p>
-            </div>
-              <h2 className="text-2xl leading-[150%] font-bold big ">
+      "
+      imgURL="https://assets-global.website-files.com/635c4eeb78332f7971255095/63d7184b737d36c33ca81413_LayoutIcon.svg"
+      mask={true}
+      >
+        <ItemsGrid />
+        </Section>
+        <Section sectionName="اتصال" title="
               ربات خود را در برنامه‌های مورد علاقه‌تان راه‌اندازی کنید.
-              </h2>
+              "
+        desc="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+        استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
+        در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد
+        نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
 
-              <p style={{color: "gray"}} className="mt-3 leading-[150%] text-base text-darker-green-grey content">
-                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
-                استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
-                در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد
-                نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
-
-              </p>
-            </Box>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <IntegrationGrid />
-          </Grid>
-        </Grid>
-        <Grid container spacing={2}>
-          <Grid
-            item
-            xs={12}
-            md={6}
-            style={{ display: "flex", flexDirection: "row" }}
-          >
-            <div className={styles.linePath}>
-              <div className={styles.boxRotate}>
-                <Image
-                  width={200}
-                  height={200}
-                  src="https://assets-global.website-files.com/635c4eeb78332f7971255095/64f7a4a5c75211da23399bfa_rocket-icon.svg"
-                  loading="lazy"
-                  alt="window icon"
-                  className="icon-path"
-                />
-              </div>
-              <motion.div 
-              initial={{ height:0 }}
-              whileInView={{
-                height:"100%"
-              }}
-              transition={{ duration: 3 , delay: 0 }}
-              className={styles.connectorPath}>
-              </motion.div>
-            </div>
-            <Box>
-            <div className="head-section ">
-              <p style={{color: "#5470ff"}} className="leading-[150%]">دانش</p>
-            </div>
-              <h2 className="text-2xl leading-[150%] font-bold big ">
-                لورم ایپسوم متن ساختگی با تولید
-              </h2>
-
-              <p style={{color: "gray"}} className="mt-3 leading-[150%] text-base text-darker-green-grey content">
-                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
-                استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
-                در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد
-                نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
-
-              </p>
-            </Box>
-          </Grid>
-          <Grid item xs={12} md={6}></Grid>
-        </Grid>
+      "
+      imgURL="https://assets-global.website-files.com/635c4eeb78332f7971255095/63d71b1e1938080cef242fea_flowIcon.svg"
+      >
+        <IntegrationGrid />
+        </Section>
       </div>
     </Container>
   );
